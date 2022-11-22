34735c0dd137b81e7f977ae5e412aff41c2593d9
Sepp <34765@ma-web.nl>
2022-11-22 12:12:25 +0100

    opdracht les1

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!");
+        let appnaam="appgame";
+        let versienummer=2.3;
+        let versiedatum=22;
+        let autheur="sepp";
+        let copyright="henks games";
+        let distributeur="epic game";
+        let darkmode=true;
+        console.log(appnaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+}
+
+let app = new App();
+app.runApplication();
