422a73df022793a5c33afee689fce4f27f094915
Sepp <34765@ma-web.nl>
2022-11-24 13:06:56 +0100

    canvas opdracht af

---
+++ les2/canvashuis/app.js
@@ -0,0 +1,59 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!");
+        let  canvas= document.getElementById("canvasId");
+        
+        let g = canvas.getContext("2d");
+        
+        g.beginPath()
+        g.fillStyle="red";
+        g.moveTo(300,100);
+        g.lineTo(200,300);
+        g.lineTo(600,400);
+        g.lineTo(700,200);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        g.beginPath()
+        g.fillStyle="gray";
+        g.moveTo(200,300);
+        g.lineTo(200,500);
+        g.lineTo(600,600);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        g.beginPath()
+        g.fillStyle="gray";
+        g.moveTo(700,200);
+        g.lineTo(800,300);
+        g.lineTo(600,400);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        g.beginPath()
+        g.fillStyle="gray";
+        g.moveTo(800,500);
+        g.lineTo(800,300);
+        g.lineTo(600,400);
+        g.lineTo(600,600);
+        
+        g.closePath();
+        g.stroke();
+        g.fill()
+        g.beginPath()
+        g.fillStyle="cyan";
+        g.moveTo(650,350);
+        g.lineTo(705,230);
+        g.lineTo(770,290);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        console.log(canvas);
+    }
+}
+
+let app = new App();
+app.runApplication();
